a7345b52693a11bcad03afaceda6f202428b95a4
JaeSeung Yang <qkffldlfdjsk12@naver.com>
2025-06-04 23:21:04 +0900

    Fix: gradle 설정 변경

---
+++ src/main/java/DiaryBackApplication.java
@@ -0,0 +1,15 @@
+package com.diary.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class DiaryBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiaryBackApplication.class, args);
+    }
+
+} 
